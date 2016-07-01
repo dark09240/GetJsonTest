42fa8215217d56cde1628517eb2d6429307dd79e
Dark <dark09240@gmail.com>
2016-07-01 17:01:47 +0800

    Initial Commit

---
+++ GetJsonTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetJsonTest
+//
+//  Created by Lycodes_Dong on 7/1/16.
+//  Copyright © 2016 Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
